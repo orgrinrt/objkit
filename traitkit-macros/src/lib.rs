e3c8cf825300df54d7a93a96069d92c988129e44
orgrinrt <ort@hiisi.digital>
2025-03-27 10:03:27 +0200

    add the missing root module

---
+++ traitkit-macros/src/lib.rs
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2025 orgrinrt (orgrinrt@ikiuni.dev)
+// SPDX-License-Identifier: MPL-2.0
+//------------------------------------------------------------------------------
+
+use include_proc_macro::macros;
+
+macros! {
+    attribute -> clone_box:: clone_box,
+}
