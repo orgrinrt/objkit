92382bd354fb77e9b194f5c41780d6fb56010f05
orgrinrt <ort@hiisi.digital>
2025-03-29 15:58:49 +0200

    add clippy rules

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,16 @@
 // SPDX-License-Identifier: MPL-2.0
 //------------------------------------------------------------------------------
 #![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/", "README.md"))]
+#![warn(
+    clippy::all,
+    clippy::pedantic,
+    clippy::nursery,
+    clippy::cargo,
+    clippy::complexity,
+    clippy::perf,
+    clippy::correctness,
+    clippy::suspicious
+)]
 
 pub use objkit_macros::as_any;
 pub use objkit_macros::as_super;
